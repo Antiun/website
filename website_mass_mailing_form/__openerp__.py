c75cd715f9fadb2f9c08f60b03c025e31ce8031b
Javier Iniesta <javieria@antiun.com>
2015-09-11 12:26:57 +0200

    Initial commit

---
+++ website_mass_mailing_form/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# See README.rst file on addon root folder for license details
+
+{
+    'name': "Website Mass Mailing Form",
+    'category': 'Website',
+    'version': '8.0.1.0.0',
+    'depends': [
+        'website',
+        'mass_mailing',
+        'website_recaptcha'
+    ],
+    'data': [
+    ],
+    'qweb': [
+    ],
+    'js': [
+    ],
+    'author': 'Antiun Ingeniería S.L., '
+              'Odoo Community Association (OCA)',
+    'website': 'http://www.antiun.com',
+    'license': 'AGPL-3',
+    'installable': True,
+}
